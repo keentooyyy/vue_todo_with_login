34dfafcafb4fda986bd6ba9c16ae28647e7b6bab
Kentoy <kentoy@gmail.com>
2024-09-30 21:52:10 +0800

    Some code cleanup

--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -4,8 +4,8 @@
   <div v-for="data in alldata">
     <p id="todo_id" class="hidden">{{ data.id }}</p>
     <p class="text-2xl">{{ data.todo_item }}</p>
-    <button @click="" class="bg-green-600 text-white mx-10 my-5 px-5 py-3">Edit</button>
-    <button @click="DeleteButton($event)" class="bg-red-600 text-white mx-10 my-5 px-5 py-3">Delete</button>
+    <button class="bg-green-600 text-white mx-10 my-5 px-5 py-3" @click="">Edit</button>
+    <button class="bg-red-600 text-white mx-10 my-5 px-5 py-3" @click="DeleteButton($event)">Delete</button>
   </div>
 </template>
 
@@ -21,8 +21,6 @@ export default {
   data() {
     return {
       alldata: [],
-
-
     };
   },
   mounted() {
@@ -31,21 +29,19 @@ export default {
   methods: {
     FetchData() {
       axios.get(url).then((res) => {
-        // console.log(res)
+
         this.alldata = res.data.data;
-        // console.log(this.alldata)
       })
     },
     DeleteButton(event) {
       const getID = document.getElementById('todo_id').innerText
-      const getParent  = event.target.parentNode;
+      const getParent = event.target.parentNode;
       getParent.remove();
       const delUrl = `${url}/${getID}`;
       axios.delete(delUrl).then((res) => {
-        // console.log(res.data.message);
         alert(res.data.message);
       })
-    //
+      //
     },
   }
 }
