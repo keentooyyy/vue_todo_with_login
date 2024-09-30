d8dd09528133266aab19580343faadeecff82f94
Kentoy <kentoy@gmail.com>
2024-09-30 21:44:54 +0800

    Succesfully shown api response and added Delete Function

--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -1,9 +1,11 @@
 <template>
   <div> TodoView</div>
   <RouterLink to="/">Go to Home</RouterLink>
-  <div v-for="a in alldata">
-<!--    <p v-for="childData in data">{{ data }}</p>-->
-    {{ a.data }}
+  <div v-for="data in alldata">
+    <p id="todo_id" class="hidden">{{ data.id }}</p>
+    <p class="text-2xl">{{ data.todo_item }}</p>
+    <button @click="" class="bg-green-600 text-white mx-10 my-5 px-5 py-3">Edit</button>
+    <button @click="DeleteButton($event)" class="bg-red-600 text-white mx-10 my-5 px-5 py-3">Delete</button>
   </div>
 </template>
 
@@ -18,7 +20,9 @@ export default {
   name: "todo",
   data() {
     return {
-      alldata: []
+      alldata: [],
+
+
     };
   },
   mounted() {
@@ -27,11 +31,22 @@ export default {
   methods: {
     FetchData() {
       axios.get(url).then((res) => {
-        // console.log(res.data)
-        this.alldata = res;
-        console.log(this.alldata)
+        // console.log(res)
+        this.alldata = res.data.data;
+        // console.log(this.alldata)
       })
-    }
+    },
+    DeleteButton(event) {
+      const getID = document.getElementById('todo_id').innerText
+      const getParent  = event.target.parentNode;
+      getParent.remove();
+      const delUrl = `${url}/${getID}`;
+      axios.delete(delUrl).then((res) => {
+        // console.log(res.data.message);
+        alert(res.data.message);
+      })
+    //
+    },
   }
 }
 </script>
